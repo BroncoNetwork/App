b7637c6b703720750ed9ef03078704430449831f
aznafro <joekwon123@yahoo.com>
2014-04-26 13:39:20 -0700

    Register Page

---
+++ BroncoNetwork/src/cs356/bronconetwork/Register.java
@@ -0,0 +1,13 @@
+package cs356.bronconetwork;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Register extends Activity {
+	
+	public void onCreate(Bundle b) {
+		super.onCreate(b);
+		setContentView(R.layout.register);
+	}
+
+}
